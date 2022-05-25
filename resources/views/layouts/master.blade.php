aa44a4b3c4df434a53be810dae86d42d70b86ea7
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-05-25 13:48:26 +0200

    first commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+{{-- Oggi create un nuovo progetto Laravel 7
+e tramite gli appositi comandi artisan
+create un model con relativa migration
+e un resource controller.
+Iniziate a definire le operazioni CRUD per gestire un archivio di fumetti.
+Bonus: creare il seeder per la tabella comics utilizzando il file in allegato. --}}
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
